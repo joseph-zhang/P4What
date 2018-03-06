5147e98868fde80618a4d33922dc26f5ccbb5d2c
metaphia <zyt5002@gmail.com>
2018-03-06 22:26:45 +0800

    Update cifarKNN.py

--- cs231n-Spring2017/cs231n/KNN/cifarKNN.py
+++ cs231n-Spring2017/cs231n/KNN/cifarKNN.py
@@ -23,13 +23,18 @@ class KNNer(object):
         num_test = X.shape[0]
         num_train = self.X_tr.shape[0]
         distance = np.zeros((num_test, num_train))
+        
+        # vectorization, calculate L2 Loss
+        # usage of np.sum
         distance = np.sqrt((-2 * np.dot(X, self.X_tr.T)) + np.sum(X**2, axis=1, keepdims=True) + np.sum(self.X_tr**2, axis=1))
 
         y_pred = np.zeros(num_test)
         for i in xrange(num_test):
             closest_y = []
-            closest_y = self.y_train[np.argsort(dists[i, :])[:k]]
-            y_pred[i] = np.argmax(np.bincount(closest_y))
+            # positions of first k min distance, then their labels
+            closest_y = self.y_tr[np.argsort(distance[i, :])[:k]] # usage of np.argsort
+            # get the position of the one which vote is maximum 
+            y_pred[i] = np.argmax(np.bincount(closest_y)) # usage of np.bincount and np.argmax
         return y_pred
 
     
