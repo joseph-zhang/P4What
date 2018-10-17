166977c394e188f8c35ed3f7b87f96a6002d980a
joseph-zhang <zyt5002@126.com>
2018-10-17 19:33:05 +0800

    update perceptron

--- PRML/perceptron.py
+++ PRML/perceptron.py
@@ -4,18 +4,18 @@ from __future__ import division
 from __future__ import print_function
 
 #------------------------------------------------------------------------------------------------------------
-#                               Naive implementation of perceptron algorithm
+#                               Naive Implementation of Perceptron Algorithm
 #------------------------------------------------------------------------------------------------------------
 import numpy as np
 
 class Perceptron(object):
     def __init__(self, eta=0.01):
-        # \eta is learning rate
+        # $\eta$ is learning rate
         self.eta = eta
         self.iter_errors = []
 
     def fit(self, X, y, num_epoch=100, info=True):
-        # \mathbf{w} = \{w_i\} + w_0
+        # $\mathbf{w} = \{w_i\} + w_0$
         self.w = np.random.rand(1+X.shape[1])
 
         for epoch in range(num_epoch):
@@ -24,7 +24,7 @@ class Perceptron(object):
                 # if gt == pred, then oe is zero
                 oe = y_i - self.inference(x_i)
 
-                # \mathbf{w}^{(\tau+1)} = \mathbf{w}^{(\tau)} + \eta\mathbf{x}_n t_n
+                # $\mathbf{w}^{(\tau+1)} = \mathbf{w}^{(\tau)} + \eta\mathbf{x}_n t_n$
                 self.w[1:] += self.eta * oe * x_i
                 self.w[0] += self.eta * oe
                 error += int(oe != 0.)
@@ -34,18 +34,18 @@ class Perceptron(object):
                             format(epoch, self.iter_errors[epoch] / X.shape[0]))
 
     def inference(self, x):
-        # y(\mathbf{x}) = f(\mathbf{w}^T\mathbf{x})
+        # $y(\mathbf{x}) = f(\mathbf{w}^T\mathbf{x})$
         return np.sign(np.dot(self.w[1:], x) + self.w[0])
 
 # Test algorithm
 if __name__ == "__main__":
-    # ------------------------------ create a test case -----------------------------
+    # ---------------------------- create a test case -----------------------------
     X = np.c_[(1.,2.), (2.,3.), (3.,3.), (2.,1.), (3.,2.)].T
     y = np.array([1.,1.,1.,-1.,-1.])
     perceptron = Perceptron(eta=0.01)
     perceptron.fit(X, y, 35, False)
 
-    # ---------------------------- draw decision boundary ---------------------------
+    # -------------------------- draw decision boundary ---------------------------
     import matplotlib.pyplot as plt
     def plot_decision_boundary(func, X = None, step=0.01):
         """
