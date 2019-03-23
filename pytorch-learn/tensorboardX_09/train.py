df8e8f609534f9e3209bfde9adba9707b02c72e8
joseph-zhang <zyt5002@126.com>
2019-03-24 00:42:37 +0800

    add tensorboardX_09

---
+++ pytorch-learn/tensorboardX_09/train.py
@@ -0,0 +1,140 @@
+import torch
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+import torchvision.utils as vutils
+from tensorboardX import SummaryWriter
+from torch.utils.data import Dataset, DataLoader
+root="./mnist/"
+
+# ---------------------------- load the dataset------------------------
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+class MyDataset(Dataset):
+    def __init__(self, image_dir, transform=None, target_transform=None, loader=default_loader):
+        fh = open(image_dir, 'r')
+        imgs = []
+        for line in fh:
+            line = line.strip('\n')
+            line = line.rstrip()
+            words = line.split()
+            imgs.append((words[0],int(words[1])))
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        fn, label = self.imgs[index]
+        img = self.loader(fn)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img,label
+
+    def __len__(self):
+        return len(self.imgs)
+
+train_data = MyDataset(image_dir = root+'train.txt', transform=transforms.ToTensor())
+test_data = MyDataset(image_dir = root+'test.txt', transform=transforms.ToTensor())
+
+train_loader = DataLoader(dataset=train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=32)
+
+
+#----------------- create the Net and training ------------------------
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 32, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2))
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv2d(32, 64, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2)
+        )
+        self.conv3 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 64, 3, 1, 1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2)
+        )
+        self.dense = torch.nn.Sequential(
+            torch.nn.Linear(64 * 3 * 3, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        conv1_out = self.conv1(x)
+        conv2_out = self.conv2(conv1_out)
+        conv3_out = self.conv3(conv2_out)
+        res = conv3_out.view(conv3_out.size(0), -1)
+        out = self.dense(res)
+        return out
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = Net().to(device)
+print(model)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0002)
+loss_func = torch.nn.CrossEntropyLoss()
+
+# create summary writer
+writer = SummaryWriter(log_dir='./ckpt_dir/')
+
+train_iter = 0
+for epoch in range(20):
+    print('epoch {}'.format(epoch + 1))
+
+    # training
+    model.train()
+    train_loss = 0.
+    train_acc = 0.
+
+    for batch_x, batch_y in train_loader:
+        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+        out = model(batch_x)
+        loss = loss_func(out, batch_y)
+        train_loss += loss.item()
+        pred = torch.max(out, 1)[1]
+        train_correct = (pred == batch_y).sum()
+        train_acc += train_correct.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_iter = train_iter + 1
+
+        # write training loss info every 20 batches
+        if train_iter % 20 == 0:
+            writer.add_scalar('train/batches_loss', loss, train_iter)
+
+    print('Train Loss: {:.6f}, Acc: {:.6f}'.format(train_loss / (len(
+        train_data)), train_acc / (len(train_data))))
+
+    # write trainig info every epoch
+    writer.add_scalar('train/acc', train_acc / len(train_data), train_iter)
+    writer.add_scalar('train/err', train_loss / len(train_data), train_iter)
+
+    # test
+    model.eval()
+    eval_loss = 0.
+    eval_acc = 0.
+    with torch.no_grad():
+        for batch_x, batch_y in test_loader:
+            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+            out = model(batch_x)
+            loss = loss_func(out, batch_y)
+            eval_loss += loss.item()
+            pred = torch.max(out, 1)[1]
+            num_correct = (pred == batch_y).sum()
+            eval_acc += num_correct.item()
+        print('Test Loss: {:.6f}, Acc: {:.6f}'.format(eval_loss / (len(
+            test_data)), eval_acc / (len(test_data))))
+
+    # write evaluation log info every epoch
+    writer.add_scalar('eval/acc', eval_acc / len(test_data), train_iter)
+    writer.add_scalar('eval/err', eval_loss / len(test_data), train_iter)
