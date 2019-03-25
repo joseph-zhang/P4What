9f8a1ced3ec2f7b878836602f16b404f43ad0806
joseph-zhang <zyt5002@126.com>
2019-03-26 00:07:34 +0800

    update files

--- pytorch-learn/GANs_10/dcgan.py
+++ pytorch-learn/GANs_10/dcgan.py
@@ -33,16 +33,16 @@ class MyDataset(Dataset):
         img = self.loader(fn)
         if self.transform is not None:
             img = self.transform(img)
-        return img,label
+        return img, label
 
     def __len__(self):
         return len(self.imgs)
 
-cust_transform = transforms.Compose([transforms.ToTensor(),
+# Resize is introduced -- torchvision > 0.1.9
+cust_transform = transforms.Compose([transforms.Resize(32),
+                                     transforms.ToTensor(),
                                      transforms.Normalize(mean=([0.5]),
                                                           std=([0.5]))])
-
-img_shape = (1, 28, 28)
 
 train_data = MyDataset(image_dir = root+'train.txt', transform=cust_transform)
 train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
@@ -50,61 +50,85 @@ train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
 
 # ------------------------------------------------------------ Network Definition -------------------------------------------------------------
 
-# define Discriminator
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-
-        self.model = nn.Sequential(
-            nn.Linear(int(np.prod(img_shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid()
-        )
-
-    def forward(self, img):
-        img_flat = img.view(img.size(0), -1)
-        validity = self.model(img_flat)
-
-        return validity
-
 # define Generator
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        def block(in_feat, out_feat, normalize=True):
-            layers = [nn.Linear(in_feat, out_feat)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_feat, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
+        self.init_size = 32 // 4
+        self.l1 = nn.Sequential(nn.Linear(100, 128*self.init_size**2))
 
-        self.model = nn.Sequential(
-            *block(100, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(img_shape))),
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, 1, 3, stride=1, padding=1),
             nn.Tanh()
         )
 
     def forward(self, z):
-        img = self.model(z)
-        img = img.view(img.size(0), *img_shape)
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
         return img
+
+# define Discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1),
+                     nn.LeakyReLU(0.2, inplace=True),
+                     nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.model = nn.Sequential(
+            *discriminator_block(1, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        # The height and width of downsampled image
+        ds_size = 32 // 2**4
+        self.adv_layer = nn.Sequential( nn.Linear(128*ds_size**2, 1),
+                                        nn.Sigmoid())
+
+    def forward(self, img):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+
+        return validity
 
 # ----------------------------------------------------------------- Training steps --------------------------------------------------------------
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
 
 # create summary writer
 writer = SummaryWriter(log_dir='./ckpt_dir/')
 
 Dnet = Discriminator().to(device)
 Gnet = Generator().to(device)
+Dnet.apply(weights_init_normal)
+Gnet.apply(weights_init_normal)
 print(Dnet)
 print(Gnet)
 
